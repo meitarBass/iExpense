a43e7c05b78a9117b29e2950f0bda93698e6a188
meitarbasson <meitarbass@gmail.com>
2022-07-14 12:25:10 +0300

    Day 36 part 1

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,13 +7,23 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+class User {
+    var firstName = "Bilbo"
+    var lastName = "Baggins"
 }
 
+struct ContentView: View {
+    @State private var user = User()
+
+    var body: some View {
+        VStack {
+            Text("Your name is \(user.firstName) \(user.lastName).")
+
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
